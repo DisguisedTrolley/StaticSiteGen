f7b83d3d5ba92a73fcbc55d17ea0d53b6a358e12
Samarth Bhat <samarthrbhat52@gmail.com>
2025-01-23 09:59:10 +0530

    Initial commit

---
+++ src/test_textnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_neq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a image node", TextType.IMAGE, "https://www.boot.dev")
+        self.assertNotEqual(node, node2)
+
+    def test_invalid_url(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertIsNotNone(node.url)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
