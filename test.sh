6791dcc899577ef9baecfa0f8aefd7480e82dde2
Samarth Bhat <samarthrbhat52@gmail.com>
2025-01-30 20:15:03 +0530

    Final workflow completed.

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-python3 -m unittest discover -s src/tests
-# python3 -m unittest src/tests/test_md_to_html.py
+#python3 -m unittest discover -s src/tests
+python3 -m unittest src/tests/test_md_to_html.py
